239667e14ed6143cdde9556aa736966e10386383
NikitaYatsura <nikitayatsura@outlook.com>
2025-03-07 21:14:35 +0200

    create func of total salary andadd git ignore

---
+++ task-1-salary.py
@@ -0,0 +1,51 @@
+from pathlib import Path
+
+def total_salary():
+
+    dir = Path(__file__).parent #get the path of current folder run script
+    salary_file = Path(dir / "salary.txt") #get the path of file with salary
+
+    # create variable for total money and dictionary of salary list
+    total_money = 0
+    salary_dict = dict()
+
+    try: 
+        # open the file safty
+        with open(salary_file, 'r', encoding = "utf-8") as file:
+
+            salary_list = file.readlines() # read the strings of salary in the file
+            salary_list = [item.strip() for item in salary_list] # remove '/n' in each lines
+
+        
+            #new_salary_list = list()
+            for i in range(len(salary_list)):
+
+                salary_list[i] = salary_list[i].split(',') # split the name of worker and salary
+              
+                # convert the list of worker and salry into the 'dict' 
+                # where 'key' is the Name of worker 'value' is his salary
+                salary_dict[salary_list[i][0]] = int(salary_list[i][1]) 
+
+             
+            # calculate the sum of the salary of all workers
+            for v in salary_dict.values():
+                total_money += v 
+
+            #calculate the average of the salary of all workers
+            average_salary = total_money // len(salary_dict.keys())
+
+            return (total_money, average_salary)
+        
+    except FileNotFoundError:
+        print("File not found")
+    except ValueError:
+        print("Salary isn't a number")
+    
+
+
+if __name__ == "__main__":
+    print(type(total_salary()))
+
+
+
+
